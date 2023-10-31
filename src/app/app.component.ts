6609c1b0f8b9081627651b5f7eae8f34c79e2806
victordnb <victordnb@gmail.com>
2023-10-31 13:23:53 +0100

    sin estilos a penas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+// app.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'padel';
+  inputs: string[] = ['', '', '', ''];
+  pairs: string[][] = [];
+
+  submit() {
+    this.pairs = [];
+    let indices = [0, 1, 2, 3];
+
+    // Empareja los inputs dos a dos aleatoriamente
+    while (indices.length > 0) {
+      let randomIndex1 = Math.floor(Math.random() * indices.length);
+      let index1 = indices[randomIndex1];
+      indices.splice(randomIndex1, 1);
+
+      let randomIndex2 = Math.floor(Math.random() * indices.length);
+      let index2 = indices[randomIndex2];
+      indices.splice(randomIndex2, 1);
+
+      this.pairs.push([this.inputs[index1], this.inputs[index2]]);
+    }
+
+    console.log(this.pairs);
+  }
 }
