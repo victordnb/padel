beea17f3f6bc2907af92b066ed7dc2b6db396a19
victordnb <victordnb@gmail.com>
2024-02-19 23:24:23 +0100

    inicio de sesion con token y registro funcionando

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 // register.component.ts
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { SharedService } from '../shared.service';
 import { Router } from '@angular/router';
@@ -10,13 +10,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  @ViewChild('registerForm', { static: false }) registerForm!: NgForm;
+
+  isFormValid = false;
 
   constructor(public sharedService: SharedService, private router: Router) { }
 
-  onSubmit(registerForm: NgForm) {
-    if (registerForm.valid) {
-      const formData = registerForm.value;
-      console.log('formData', registerForm);
+  onSubmit() {
+    this.isFormValid = this.registerForm.valid || false;
+    if (this.isFormValid) {
+      const formData = this.registerForm.value;
+      console.log('formData', this.registerForm);
 
       this.sharedService.register(formData).subscribe((response: any) => {
         if (response) {
